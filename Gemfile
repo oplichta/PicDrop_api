1bba1cbaeb4125e3fa5f38f7a9a71d01cfadd5fe
Oskar Plichta <osi22@o2.pl>
2015-08-20 13:37:34 +0200

    Add devise by JSON authorization

--- Gemfile
+++ Gemfile
@@ -21,7 +21,14 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'devise'
-
+gem 'koala'
+gem 'twitter'
+gem 'flickraw'
+gem 'omniauth'
+gem 'omniauth-twitter'
+gem 'omniauth-facebook'
+gem 'omniauth-flickr'
+gem "rack-cors", require: "rack/cors"
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150819122008) do
+ActiveRecord::Schema.define(version: 20150819124305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20150819122008) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "user_name"
+    t.string   "authentication_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
